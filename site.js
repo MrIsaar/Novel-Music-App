bf4116c4053f5b35d5948e48b71949fa145c5587
MrIsaar <47126870+MrIsaar@users.noreply.github.com>
2022-02-16 13:49:10 -0700

    update ball drop test

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
@@ -13,6 +13,8 @@ var Engine = Matter.Engine,
 
 var balls = [];
 var slots = [];
+var height = 1000;
+var width = 1000;
 
 // create an engine
 var engine = Engine.create();
@@ -20,28 +22,32 @@ var engine = Engine.create();
 // create a renderer
 var render = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine, options: {
+        width: width,
+        height: height,
+        wireframes: false
+    }
 });
 
 // create two boxes and a ground
-let maxBalls = [20 , 5];
+let maxBalls = [40 , 7];
 for (let i = 0; i < maxBalls[0]; i++) {
     for (let j = 0; j < maxBalls[1]; j++) {
-        balls.push(Bodies.circle(((j % 2) * (500 / maxBalls[0])) + i * (1000/maxBalls[0]), j * 20, 10, { restitution: 1 }));
+        balls.push(Bodies.circle(((j % 2) * (500 / maxBalls[0])) + i * (1000/maxBalls[0]), j * 20, 10, { restitution: 0.9  , friction: 0.001}));
     }
 }
 /*var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);*/
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
+var ground = Bodies.rectangle(width/2, height, width, 50, { isStatic: true });
+var funnel = [Bodies.rectangle(0, 100, width - 20, 10, { isStatic: true, angle: 3.14 / 10, friction: 0 }), Bodies.rectangle(width, 100, width - 20, 10, { isStatic: true, angle: -3.14 / 10, friction: 0})];
 
 //var midBox = Bodies.rectangle(400, 500, 60, 60, { isStatic: true, angle: 3.14 / 4 });
 
 let maxSlots = 20;
 for (let i = 0; i < maxSlots; i++) {
-    slots.push(Bodies.rectangle(0 + i * (1000 / maxSlots), 600, 20, 300, { isStatic: 1 }));
-    for (let j = 0; j < 6; j++) {
-        slots.push(Bodies.rectangle((25 * (j % 2)) + i * (1000 / maxSlots), 450 - (j * 50), 15, 15, { isStatic: true, angle: 3.14 / 4 }));
+    slots.push(Bodies.rectangle(0 + i * (1000 / maxSlots), height - 10, 15, 350, { isStatic: 1 }));
+    for (let j = 0; j < 11; j++) {
+        slots.push(Bodies.rectangle((25 * (j % 2)) + i * (1000 / maxSlots), height - 190 - (j * 50), 10, 10, { isStatic: true, angle: 3.14 / 4 }));
     }
     /*slots.push(Bodies.rectangle(25 + i * (1000 / maxSlots), 400, 15, 15, { isStatic: true, angle: 3.14 / 4 }));
     slots.push(Bodies.rectangle(0 + i * (1000 / maxSlots), 350, 15, 15, { isStatic: true, angle: 3.14 / 4 }));
@@ -52,6 +58,7 @@ for (let i = 0; i < maxSlots; i++) {
 
 // add all of the bodies to the world
 Composite.add(engine.world, [ground]);
+Composite.add(engine.world, funnel);
 Composite.add(engine.world, balls);
 Composite.add(engine.world, slots);
 
