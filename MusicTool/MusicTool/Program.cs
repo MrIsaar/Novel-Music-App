300817d134302907e00f196a1c9d6c991b1e19f0
Jackson Dean <jacksondean17@gmail.com>
2022-04-20 13:43:59 -0600

    db scaffolding

--- MusicTool/MusicTool/Program.cs
+++ MusicTool/MusicTool/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MusicTool.Areas.Identity.Data;
+using MusicTool.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MusicToolContextConnection");;
+
+builder.Services.AddDbContext<MusicToolContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<MusicToolUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<MusicToolContext>();;
 
 // Add services to the container.
 
@@ -23,5 +34,36 @@ app.MapControllerRoute(
     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html"); ;
+app.UseAuthentication();;
+
+await CreateDbIfNotExistsAsync(app);
 
 app.Run();
+
+
+static async Task CreateDbIfNotExistsAsync(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = host.Services;
+        try
+        {
+            var musicToolDB = services.GetRequiredService<MusicToolContext>();
+
+            //UserManager<MusicToolUser> um = services.GetRequiredService<UserManager<MusicToolUser>>();
+            //RoleManager<IdentityRole> rm = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            //await SeedUsersRolesDB.InitializeAsync(rolesDB, um, rm);
+            musicToolDB.Database.EnsureCreated();
+
+
+            var TADB = services.GetRequiredService<MusicToolContext>();
+            //TA_DB_Initializer.Initialize(TADB, rolesDB);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
