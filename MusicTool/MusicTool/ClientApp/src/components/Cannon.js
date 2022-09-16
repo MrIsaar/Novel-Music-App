9e13cc72f14f7d7381bd4d7ceb0638378e4ff9e1
Carter Morgan <carter.r.morgan@gmail.com>
2022-09-16 01:01:47 -0600

    Fix cannon constructor signature
    Was missing parameter for collision filter

--- MusicTool/MusicTool/ClientApp/src/components/Cannon.js
+++ MusicTool/MusicTool/ClientApp/src/components/Cannon.js
@@ -23,7 +23,7 @@ const CANNON_SHAPE = [{ x: -20, y: 20 }, { x: 40, y: 0 }, { x: -20, y: -20 }, { 
      * @param {any} marbleSize default 20
      * @param {any} marbleCollisionFilter default is all
      */
-     constructor(pos, angle = 0, power = 20, fireOn = -1, marbleColor = "rand", marbleSize = 20) {
+   constructor(pos, angle = 0, power = 20, fireOn = -1, marbleColor = "rand", marbleSize = 20, marbleCollisionFilter = { group: -1, category: 0xFFFFFFFF, mask: 0xFFFFFFFF }) {
         super();
         this.body = Matter.Bodies.fromVertices(pos.x, pos.y, CANNON_SHAPE, { angle: angle,render: { fillStyle: 'red' }, isStatic: true, collisionFilter: { group: 0, category: 0, mask: 0 } });
         this.fireOn = fireOn;
