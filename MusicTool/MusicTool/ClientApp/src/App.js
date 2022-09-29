113f7b408c7280385e853401decb3b9223979a9d
Carter Morgan <carter.r.morgan@gmail.com>
2022-09-29 12:34:38 -0600

    Remove unnecessary imports

--- MusicTool/MusicTool/ClientApp/src/App.js
+++ MusicTool/MusicTool/ClientApp/src/App.js
@@ -2,15 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Scene } from './components/Scene';
-import { Cannon } from './components/Cannon';
-import { ToneExample } from './components/ToneSetup'
-import { Selection } from './components/Selection'
-import { SequencerDemo } from './components/SequencerDemo';
-import { PalletDemo } from './components/PalletDemo';
-import Overlay from 'react-bootstrap/Overlay';
 import { DBTest } from './components/DBTest';
 import { Login } from './components/Login';
 
