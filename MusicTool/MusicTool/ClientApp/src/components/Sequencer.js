e7b02b3c27d71ab48c72c95772d78c3f378dfcbd
Carter Morgan <carter.r.morgan@gmail.com>
2022-11-03 21:40:59 -0600

    Implement selected track changing and rendering

--- MusicTool/MusicTool/ClientApp/src/components/Sequencer.js
+++ MusicTool/MusicTool/ClientApp/src/components/Sequencer.js
@@ -105,6 +105,11 @@ export class Sequencer extends Component {
         this._noteMatrix.splice(trackIndex, 1);
         this._trackIDs.splice(trackIndex, 1);
         this._trackNames.splice(trackIndex, 1);
+
+        // If this was the selected track, reset selected track to default.
+        if (trackID === this.props.selectedTrack) {
+            this.props.onSelectedTrackChange();
+        }
         
         //for (let i = 0; i < this.state.numTracks; i++) {
         //    if (this._trackIDs[i] != trackID) { // dont copy the track to be removed
