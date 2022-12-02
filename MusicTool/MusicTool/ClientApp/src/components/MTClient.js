a640e40d4823960af2b0a3ce7c1744253c8c0894
Map-xs <jxd1340013487@gmail.com>
2022-12-01 17:37:27 -0700

    Beautify buttons

--- MusicTool/MusicTool/ClientApp/src/components/MTClient.js
+++ MusicTool/MusicTool/ClientApp/src/components/MTClient.js
@@ -126,9 +126,12 @@ export class MTClient extends React.Component {
                     <div className="col-3">
 
 
-                        <button onClick={this.handleSave} id="saveToDBButton">SAVE-CREATION</button>
+                        <Button variant="primary" onClick={this.handleSave} id="saveToDBButton">SAVE-CREATION</Button>
 
-                        <button onClick={() => { this.deleteObject(this.scene.selection != null ? this.scene.selection.selected : null); }} id="deleteToDBButton" >DELETE-OBJECT</button>
+                        <br></br>
+                        <br></br>
+
+                        <Button variant="primary" onClick={() => { this.deleteObject(this.scene.selection != null ? this.scene.selection.selected : null); }} id="deleteToDBButton" >DELETE-OBJECT</Button>
                         <div >
                            
                             <div id="cannonSettings">
