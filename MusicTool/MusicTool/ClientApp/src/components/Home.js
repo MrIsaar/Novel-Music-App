6cbdf6d0f6225acafbba9f9e481a628ec7251279
Map-xs <jxd1340013487@gmail.com>
2022-12-02 19:33:42 -0700

    Resize

--- MusicTool/MusicTool/ClientApp/src/components/Home.js
+++ MusicTool/MusicTool/ClientApp/src/components/Home.js
@@ -5,8 +5,8 @@ export class Home extends Component {
   static displayName = Home.name;
 
   render () {
-    return (
-        <div>
+      return (
+          <div style={{ width: 1300, height: 900 }}>
             <h1>Welcome to Beat Drop Music Tool !</h1>
             <iframe src="UserGuide.html"></iframe>
         </div>
